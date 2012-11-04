c8d9ca96162375f061242f82d3fba0de66436d1e
qiye <lijinxing@gmail.com>
2012-11-04 23:06:24 +0800

    update redis2.6.2

--- src/release.h
+++
@@ -1,2 +0,0 @@
-#define REDIS_GIT_SHA1 "a777cdf0"
-#define REDIS_GIT_DIRTY "24"
--- tests/unit/introspection.tcl
+++
@@ -1,5 +0,0 @@
-start_server {tags {"introspection"}} {
-    test {CLIENT LIST} {
-        r client list
-    } {*addr=*:* fd=* idle=* flags=N db=9 sub=0 psub=0 qbuf=0 obl=0 oll=0 events=r cmd=client*}
-}
